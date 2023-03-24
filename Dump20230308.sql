6b817d4eebc45a2e48d2a2f85528b2da20b3f425
pmgespina <pmgespina@uma.es>
2023-03-24 10:42:43 +0100

    dump updated

--- Dump20230308.sql
+++ Dump20230308.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `NutritionalDB` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `NutritionalDB`;
 -- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: NutritionalDB
@@ -207,4 +209,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-23 23:42:58
+-- Dump completed on 2023-03-24 10:42:26
